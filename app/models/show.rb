f0a1b9632c771df844af9598c5b78d0526070813
Paul Cohn <pscohn@gmail.com>
2015-09-06 20:58:53 -0400

    get next episode

--- app/models/show.rb
+++ app/models/show.rb
@@ -21,4 +21,26 @@ class Show < ActiveRecord::Base
     end
     count
   end
+
+  def next_episode
+    #TODO make better
+    episode = seasons.find_by(number: 1).episodes.find_by(number: 1)
+    last_watched = nil
+    seasons.each do |season|
+      season.episodes.each do |episode|
+        if episode.watched
+          last_watched = episode
+        end
+      end
+    end
+    if last_watched
+      next_in_season = last_watched.season.episodes.find_by(number: last_watched.number + 1)
+      if next_in_season.nil?
+        episode = seasons.find_by(number: last_watched.season.number + 1).episodes.find_by(number: 1)
+      else
+        episode = next_in_season
+      end
+    end
+    episode
+  end
 end
