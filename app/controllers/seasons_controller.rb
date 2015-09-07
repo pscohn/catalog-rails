f33cd6ffe9139930b3f9f083c9cc07996d0da1f4
Paul Cohn <pscohn@gmail.com>
2015-09-06 20:20:48 -0400

    create multiple episodes when creating season

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -2,6 +2,12 @@ class SeasonsController < ApplicationController
   def create
     @show = Show.find(params[:show_id])
     @season = @show.seasons.create(season_params)
+    if params[:number_of_episodes] != ""
+      num = params[:number_of_episodes].to_i
+      num.times do |i|
+        @season.episodes.create(number: i+1, watched: false)
+      end
+    end
     redirect_to show_path(@show)
   end
 
